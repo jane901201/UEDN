d0f71a8d1d4e779a273041186f8edcf68707e3cf
MSI\HORUSHIUAN <jane36742715@gmail.com>
2025-11-19 05:46:02 +0800

    完成調整音樂與SFX大小的功能

---
+++ Source/UEDN/ELanguage.h
@@ -0,0 +1,14 @@
+﻿// ELanguage.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ELanguage.generated.h"
+
+UENUM(BlueprintType)
+enum class ELanguage : uint8
+{
+	TraditionalChinese  UMETA(DisplayName = "繁體中文"),
+	SimplifiedChinese   UMETA(DisplayName = "简体中文"),
+	English             UMETA(DisplayName = "English"),
+	Japanese            UMETA(DisplayName = "日本語")
+};
